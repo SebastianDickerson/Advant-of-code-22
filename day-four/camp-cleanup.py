778fbf97a96c46f34f2209513a2f7267ab518d9e
SebastianDickerson <65762746+SebastianDickerson@users.noreply.github.com>
2022-12-05 17:22:01 +1000

    day 4 complete

---
+++ day-four/camp-cleanup.py
@@ -0,0 +1,36 @@
+def match_pairs(option, a, b):
+    options = {
+        "any": any,
+        "all": all
+    }
+    match = options.get(option.lower())
+
+    return match(item in a for item in b) or match(item in b for item in a)
+
+with open('data/raw-data.txt') as f:
+    lines = f.read().splitlines() 
+    all_matched_pairs = []
+    any_matched_pairs = []
+
+    for idx, line in enumerate(lines):
+        pairs = line.split(',')
+        f_pair = [pair.split('-') for pair in pairs]
+        first_elf = [item for item in range(int(f_pair[0][0]), int(f_pair[0][1]) + 1)]
+        second_elf = [item for item in range(int(f_pair[1][0]), int(f_pair[1][1]) + 1)]
+        
+        a = all(item in first_elf for item in second_elf)
+        b = all(item in second_elf for item in first_elf)
+        c = any(item in first_elf for item in second_elf)
+        d = any(item in second_elf for item in first_elf)
+
+
+        if match_pairs("all", first_elf, second_elf):
+            all_matched_pairs.append([idx, line])
+        
+        if match_pairs("any", first_elf, second_elf):
+            any_matched_pairs.append([idx, line])
+
+    # part 1
+    print(len(all_matched_pairs))
+    # part 2
+    print(len(any_matched_pairs))
